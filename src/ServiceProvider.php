99b9496b9239b38f4ab5a10e2ba06694a07a0fa4
Dan <act.dcisneros@gmail.com>
2022-07-31 22:16:27 +0800

    [UPDATE] service provider all

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -22,7 +22,19 @@ class ServiceProvider extends SP
             __DIR__ . '/defaults/.env.setup' => config_path('../.env.setup'),
             __DIR__ . '/defaults/phpcs.xml' => config_path('../phpcs.xml'),
             __DIR__ . '/defaults/gitignore' => config_path('../.gitignore'),
-            __DIR__ . '/SlackLog/config/slackLog.php' => config_path('./slackLog.php')
+            __DIR__ . '/SlackLog/config/slackLog.php' => config_path('./slackLog.php'),
+            __DIR__ . '/defaults/resources/lang/en/words.php' => config_path('../' . $langPath . 'lang/en/words.php'),
+            __DIR__ . '/defaults/resources/lang/en/messages.php' => config_path('../' . $langPath . 'lang/en/messages.php'),
+            __DIR__ . '/defaults/resources/lang/ja/auth.php' => config_path('../' . $langPath . 'lang/ja/auth.php'),
+            __DIR__ . '/defaults/resources/lang/ja/pagination.php' => config_path('../' . $langPath . 'lang/ja/pagination.php'),
+            __DIR__ . '/defaults/resources/lang/ja/passwords.php' => config_path('../' . $langPath . 'lang/ja/passwords.php'),
+            __DIR__ . '/defaults/resources/lang/ja/validation.php' => config_path('../' . $langPath . 'lang/ja/validation.php'),
+            __DIR__ . '/defaults/resources/lang/ja/words.php' => config_path('../' . $langPath . 'lang/ja/words.php'),
+            __DIR__ . '/defaults/resources/lang/ja/messages.php' => config_path('../' . $langPath . 'lang/ja/messages.php'),
+            __DIR__ . '/defaults/Traits/Models/ParentModel.php' => config_path('../Traits/Models/ParentModel.php'),
+            __DIR__ . '/defaults/app/Models/MainModel.php' => config_path('../app/Models/MainModel.php'),
+            __DIR__ . '/defaults/app/Models/MainModelAuthenticatable.php' => config_path('../app/Models/MainModel.php'),
+            __DIR__ . '/defaults/app/Models/MainModelCompoships.php' => config_path('../app/Models/MainModelCompoships.php')
         ], 'all');
 
         $this->publishes([
@@ -85,7 +97,7 @@ class ServiceProvider extends SP
             __DIR__ . '/defaults/Traits/Models/ParentModel.php' => config_path('../Traits/Models/ParentModel.php'),
             __DIR__ . '/defaults/app/Models/MainModel.php' => config_path('../app/Models/MainModel.php'),
             __DIR__ . '/defaults/app/Models/MainModelAuthenticatable.php' => config_path('../app/Models/MainModel.php'),
-            __DIR__ . '/defaults/app/Models/MainModelCompoships.php' => config_path('../app/Models/MainModelCompoships.php'),
+            __DIR__ . '/defaults/app/Models/MainModelCompoships.php' => config_path('../app/Models/MainModelCompoships.php')
         ], 'model');
     }
 }
